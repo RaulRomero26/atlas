53c0aa52657d991564afcb13bd8ed39bff4f3f24
Raúl Romero <link_2642@hotmail.com>
2022-08-02 17:18:21 -0500

    Activo centrado

--- application/views/system/gestorCasos/atlaspdf-individual.php
+++ application/views/system/gestorCasos/atlaspdf-individual.php
@@ -157,7 +157,9 @@ for($cantidad_grupo=0;$cantidad_grupo<count($data);$cantidad_grupo++){
             $pdf->SetY(50);
             $pdf->SetX(63);
             $pdf->SetTextColor(255, 0, 0);
-            $pdf->Cell(5,4,utf8_decode($data[$cantidad_grupo]['integrantes'][$contador_integrantes]->ESTATUS));
+            $pdf->SetFont('Arial','B',12);
+            $pdf->Cell(41,4,utf8_decode($data[$cantidad_grupo]['integrantes'][$contador_integrantes]->ESTATUS),0,0,'C');
+            $pdf->SetFont('Arial','',12);
             $pdf->SetTextColor(31, 56, 100);
             $pdf->SetY(92);
             $pdf->SetX(48);
